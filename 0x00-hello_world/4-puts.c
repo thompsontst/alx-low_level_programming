869bed642c34ad9fbf7c3a28872f72ebf83dace4
thompson <thompsonabove@gmail.com>
2022-09-10 11:48:26 -0700

    helloworld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main (void){/*The main function is the used in order to ascertain the program class*/
+/*calling the main function where the source code will be executed*/
+int main(void){
 puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }

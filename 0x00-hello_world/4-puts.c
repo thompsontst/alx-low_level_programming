c3af21658fae57e4e6a84e4c3e4746ae3ba72133
thompson <thompsonabove@gmail.com>
2022-09-10 12:29:40 -0700

    helloworld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * this program prints "programming is like building a multiligual puzzle
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0); 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

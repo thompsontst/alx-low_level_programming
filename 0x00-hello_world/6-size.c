3ccbee33ffce744fcafd33e04728fa5e5c75cd72
thompson <thompsonabove@gmail.com>
2022-09-11 14:10:58 -0700

    okay

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char charaacter;
 	float floatingtype;
 	long long int longerinteger;
-printf("Size of a char: %ld\n,%c\n", sizeof(char), byte(s));
-printf("Size of an int: %ld\n%c\n", sizeof(int), byte(s));
-printf("Size of a long int: %ld\n%c\n", sizeof(long int), byte(s));
-printf("Size of a long long int: %ld\n%c\n", sizeof(long long int), byte(s));
-printf("Size of a float: %ld\n%c\n", sizeof(float), byte(s));
+printf("Size of a char: %ld-byte(s)\n", sizeof(char), byte(s));
+printf("Size of an int: %ld-byte(s)", sizeof(int), byte(s));
+printf("Size of a long int: %ld-byte(s)\n", sizeof(long int), byte(s));
+printf("Size of a long long int: %ld-byte(s)\n", sizeof(long long int), byte(s));
+printf("Size of a float: %ld-byte(s)\n", sizeof(float), byte(s));
 return (0);
 }

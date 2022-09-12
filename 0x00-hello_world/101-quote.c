34e0cceb8c470a8ad382fc305abfd68963e65675
thompson <thompsonabove@gmail.com>
2022-09-11 19:52:35 -0700

    okay

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - uses the stderr function
+ * Description: main function
+ * this program prints a string to the standard error.
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr, " and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

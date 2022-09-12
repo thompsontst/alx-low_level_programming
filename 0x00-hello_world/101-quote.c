2d9836851d5db0319ef3b8b92c4590b9d269a7d9
thompson <thompsonabove@gmail.com>
2022-09-11 21:12:28 -0700

    okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - uses the stderr function
  * Description: main function
@@ -8,6 +8,6 @@
 int main(void)
 {
 char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-fprintf(stderr, str);
+write(2, str1, 59);
 return (1);
 }

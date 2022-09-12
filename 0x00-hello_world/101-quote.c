b33eed4a9f01d669e2191bfe3c214f621ea27885
thompson <thompsonabove@gmail.com>
2022-09-11 20:06:11 -0700

    okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - uses the stderr function
  * Description: main function
- * this program prints a string to stderr
+ * this program prints a string to stderir
  * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }

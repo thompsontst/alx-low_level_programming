8da2a003c5ed6d9fa075863751ab77db5bc81515
thompson <thompsonabove@gmail.com>
2022-09-12 15:48:55 -0700

    lettersofthealphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,8 +9,8 @@ int main(void)
 	char lettersofthealphabet;
 	for(lettersofthealphabet = 'a'; lettersofthealphabet <= 'z'; lettersofthealphabet++)
 	{
-		printchar(lettersofthealphabet);
+		putchar(lettersofthealphabet);
 	}
-	printchar('\n');
+	putchar('\n');
 	return (0);
 }

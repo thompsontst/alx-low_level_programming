507530240c5539e6870a7d778d4b069f0fb3941c
thompson <thompsonabove@gmail.com>
2022-09-12 13:48:24 -0700

    good

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,16 +24,16 @@ int main(void)
 
 	if (num > 5)
 	{
-	printf("and is greater than 5\n");
+	printf(" and is greater than 5\n");
 
 	        }            
 	else if (num == 0)
 {
-	printf("and is 0\n");
+	printf(" and is 0\n");
 }
 else
 {
-	printf("and is less than 6 and not 0\n");
+	printf(" and is less than 6 and not 0\n");
 }
 	return (0);
 }

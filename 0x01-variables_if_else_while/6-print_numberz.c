f84b461fbd40e177c763eb34671271e0c7ebfad6
thompson <thompsonabove@gmail.com>
2022-09-12 23:32:22 -0700

    numbers_in_base_ten

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char num ='0';
+	int num;
 
-	for (num = '0'; num < '10'; num++)
-	
-		putchar(num);
-	
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num%10) + '0');
+	}
 	putchar('\n');
 	return (0);
 }

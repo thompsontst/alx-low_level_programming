2dd9954587de5f497607b2c5357c51e6060e99d9
thompson <thompsonabove@gmail.com>
2022-09-12 22:18:59 -0700

    lower_and_uppercase_without_e_and_q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 	char alphabets;
+
 	for ( alphabets = 'a'; alphabets <= 'z'; alphabets++)
 	{
-		if (alphabets != 'e' && alphabets != 'q');
+		if (alphabets != 'e' && alphabets != 'q')
 			putchar(alphabets);
 	}
 		putchar('\n');

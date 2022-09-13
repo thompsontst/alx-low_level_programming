9765b603aa71f7e007b8799835b76a922de135cc
thompson <thompsonabove@gmail.com>
2022-09-12 23:01:49 -0700

    numbers_in_base_ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all single digit in base 10 starting from zero
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}

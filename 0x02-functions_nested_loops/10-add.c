98237c9c2339aff15f9fb23ef708e7e73adff00a
thompson <thompsonabove@gmail.com>
2022-09-14 16:11:36 -0700

    okay

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - entry point
+* @c: first integer
+* @d: second integer
+* Description: A program that displays the 24 hours time.
+* Return: void
+*/
+int add(int c, int d )
+{
+	return (a + b);
+}

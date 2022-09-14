0ae7d5a21381768144ff02a72af52f3c1c675525
thompson <thompsonabove@gmail.com>
2022-09-14 00:52:53 -0700

    okay

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,15 @@ void print_alphabet_x10(void)
   char letters, space;
   
  for (letters = 0; letters <= 9; letters++)
-{
+   {
   
-  for (space = 'a'; space <= 'z'; space++)
-    {
+     for (space = 'a'; space <= 'z'; space++)
+       {
       
     _putchar(letters);
- }
+       }
   
   _putchar('\n');
- }
- 
    }
+ 
+}

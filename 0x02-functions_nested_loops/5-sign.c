58360ac59393181d21d811700e925c0b99e016bd
thompson <thompsonabove@gmail.com>
2022-09-14 06:15:01 -0700

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* _print_sign - entry point
+* @c: the letter the check is conducted on
+* main - entry point
+* Description: A function that prints that checks for lowercase,
+* int _islower(int c): returns 1 if true and 0 if false.
+* Return: 0
+*/
+int print_sign(int n);
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if(n < 0)
+{
+_putchar('-');
+	return (-1);
+}
+else
+{
+	_putchar('0');
+return ('-1');
+}
+_putchar('\n');
+}

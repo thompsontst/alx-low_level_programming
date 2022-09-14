fab923cea6c3a0c476e27a8273acc461706cb6ec
thompson <thompsonabove@gmail.com>
2022-09-14 00:14:39 -0700

    okay

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+* main - entry point
+* Description: A function that prints 10 times the alphabet, in lowercase, followed by a new line.
+* Return: 0
+*/
+int main(void)
+{
+char letters;
+ int space;
+ for (letters = 'a'; letters <= 'z'; letters++)
+{
+  int space = 0;
+  for (space = 0; space <= 10; space++)
+    {
+    _putchar("\n");
+ }
+  _putchar(letters);
+ }
+ return (0);
+   }

6c2eb934b547abaefc57e7f7d088f46157059724
thompson <thompsonabove@gmail.com>
2022-09-14 15:14:55 -0700

    okay

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <limits.h>
+/**
+* main - entry point
+* Description: prints the first 98 Fibonacci numbers, starting with 1 and 2
+* Return: 0.
+*/
+int main(void)
+{
+	unsigned long int d;
+	unsigned long int bef =1;
+	unsigned long int aft = 2;
+
+	printf("%lu", bef);
+
+	for (d = 1; d < 50; d++)
+	{
+		printf(", %lu", aft);
+
+		aft += bef;
+		bef = aft - bef;
+	}
+	print("\n");
+	return (0);
+}

5c70382e633f9c928769b35dfb0ec1971fd77117
thompson <thompsonabove@gmail.com>
2022-09-14 01:50:19 -0700

    int _islower(int c);

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* int _islower(int c) - entry point
+* main - entry point
+* Description: A function that prints 10 times the alphabet,
+* in lowercase, followed by a new line.
+* print_alphabet_x10: print abc 10 times
+* Return: 0
+*/
+int _islower(int c)
+{
+int space = 'c';
+if ( islower(space) )
+{
+	return (1);
+}
+else 
+return (0);
+}
+_putchar('\n');
+}

2a007031092dd04f4588dfdd2417c15d206f437f
thompson <thompsonabove@gmail.com>
2022-09-15 13:35:57 -0700

    okay

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,6 @@
 #ifndef _H_MAIN
 #define _H_MAIN
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <elf.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <unistd.h>
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -21,13 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-ssize_t read_textfile(const char *filename, size_t letters);
-int creat_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
 #endif

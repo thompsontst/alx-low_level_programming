0d3ac01ddfaed55453ca099f2beb3536cbb3b557
thompson <thompsonabove@gmail.com>
2022-09-15 14:54:02 -0700

    okay

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+project on debugging.
+its intended to test sudent understanding of the process of code debugging
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef _H_MAIN
+#define _H_MAIN
+
+void positive_or_negative(i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */

9a402e2d6f0e66134b8d1f620c96373b865696b1
thompson <thompsonabove@gmail.com>
2022-09-15 13:05:10 -0700

    okay

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 /**
 * print_numbers - entry point
 * Description: A function  that checks for digits,
-* andprints numbers 1 to 9
+* andprints numbers 1 to 9.
 * Return: void
 */
 void print_numbers(void)

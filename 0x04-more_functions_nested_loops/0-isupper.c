4a10031f874f8c14d32f88b2d9b8bfd0ca659f32
thompson <thompsonabove@gmail.com>
2022-09-15 10:25:08 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _isupper - entry point
+* @c: the letter the check is conducted on
+* main - entry point
+* Description: A function that prints that checks for uppercase,
+* int _islower(int c): returns 1 if true and 0 if false.
+* Return: 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - entry point
+* main - entry point
+* Description: A function that prints 10 times the alphabet,
+* in lowercase, followed by a new line.
+* print_alphabet_x10: print abc 10 times
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char letters, space;
+for (letters = 0; letters <= 9; letters++)
+{
+for (space = 'a'; space <= 'z'; space++)
+{
+_putchar(space);
+}
+_putchar('\n');
+}
+}

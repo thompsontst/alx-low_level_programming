dc569bd79fde92445afce230665422836eb86564
thompson <thompsonabove@gmail.com>
2022-09-15 11:43:42 -0700

    okay

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _isdigit - entry point
+* @c: the letter the check is conducted
+* main - entry point
+* Description: A function  that checks for digits,
+* int _islower(int c): returns 1 if true and 0 if false.
+* Return: 0
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+	return (1);
+else
+return (0);
+
+}

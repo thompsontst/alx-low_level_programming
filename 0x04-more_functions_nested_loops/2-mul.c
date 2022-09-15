12a91fa1ee3bf024a03087cacd13c82471a16971
thompson <thompsonabove@gmail.com>
2022-09-15 12:00:24 -0700

    okay

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _mul - entry point
+* mul - entry point
 * @a: the first integer
 * @b: the second integer
 * main - entry point
 * Description: A function  that checks for digits,
 * int _islower(int c): returns 1 if true and 0 if false.
-* Return: 0
+* Return: the product
 */
 int mul(int a, int b)
 {

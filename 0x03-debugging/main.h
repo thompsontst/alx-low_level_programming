1e3ef8930bb4699ac8eee815dd61e597ba559b8b
thompson <thompsonabove@gmail.com>
2022-09-15 23:49:45 -0700

    okay

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef _H_MAIN
 #define _H_MAIN
 
-void positive_or_negative(i);
+int positive_or_negative( int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int month, int day);

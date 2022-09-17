ea93bc289a40d3d875d8f4ed4202072603421ac3
anithomp <thompsonabove@gmail.com>
2022-09-17 20:37:08 +0100

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_most_numbers - entry point
+*/
+void print_most_numbers(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+_putchar (x + '0');
+}
+_putchar('\n');
+}

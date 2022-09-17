51163989470ba0f9bbf0ea43ff757b5f465cb1a8
anithomp <thompsonabove@gmail.com>
2022-09-17 21:00:41 +0100

    good

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,10 @@
 void more_numbers(void)
 {
 int letters, space;
-for (letters = 0; letters < 15; letters++)
+
+for (letters = 0; letters < 11; letters++)
 {
-for (space = 0; space < 11; space++)
+for (space = 0; space < 15; space++)
 {
 _putchar(space);
 }

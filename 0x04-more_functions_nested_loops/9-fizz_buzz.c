51d7ddbdc4b549f8235005e79a3d9e015b0cf37f
anithomp <thompsonabove@gmail.com>
2022-09-17 22:22:41 +0100

    ioio

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,24 +17,26 @@ int n;
 
 for (n = 0; n < 101; n++)
 {
-if(100 % 15 == 0)
+if(n % 15 == 0)
 {
 printf("FizzBuzz");
 }
-else if(100 % 5 == 0)
+else if(n % 5 == 0)
 {
 printf("Buzz");
 }
-else if(100 % 3 == 0)
+else if(n % 3 == 0)
 {
 printf("Fizz");
 }
 else
-{
+
 printf("%d", n);
-}
+
 if (n != 100)
+
 printf(" ");
+
 }
 printf("\n");
 return (0);

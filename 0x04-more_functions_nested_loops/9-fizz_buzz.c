b78c5c8962ea8201a0a5b777db357930f851ae0f
anithomp <thompsonabove@gmail.com>
2022-09-17 22:27:07 +0100

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-* @n: Starting point 
+* main - entry point 
 * Description: A function that prints 0 t0 100, buzz for multiples of 5,
 * fizz for multiples of 3
 * fizzbuzz for multiples of 15,
@@ -15,17 +14,17 @@ int main(void)
 {
 int n;
 
-for (n = 0; n < 101; n++)
+for (n = 1; n < 101; n++)
 {
-if(n % 15 == 0)
+if (n % 15 == 0)
 {
 printf("FizzBuzz");
 }
-else if(n % 5 == 0)
+else if (n % 5 == 0)
 {
 printf("Buzz");
 }
-else if(n % 3 == 0)
+else if (n % 3 == 0)
 {
 printf("Fizz");
 }

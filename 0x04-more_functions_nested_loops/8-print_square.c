537c9f31431345238e338fb492b6b887d148156c
anithomp <thompsonabove@gmail.com>
2022-09-18 22:34:05 +0100

    print_square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,13 @@
 */
 void print_square(int size)
 {
-int line;
+int line, space;
 if (size <= 0)
+_putchar('\n');
 for (line = 0; line < size; line++)
 {
+for (space = 0; space <= line; space++)
+
 _putchar('#');
 _putchar('\n');
 }

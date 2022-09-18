19752017adff011fbca15be8848974e9ff3224fe
anithomp <thompsonabove@gmail.com>
2022-09-18 20:04:42 +0100

    okay

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,14 +11,14 @@ int letters, numbers, values;
 
 for (letters = 0; letters <= 7; letters++)
 {
-for (numbers = letters + 1; numbers <= 7; numbers++)
+for (numbers = letters + 1; numbers <= 8; numbers++)
 {
-for (values = letters + 1; values <= 7; values++)
+for (values = letters + 1; values <= 9; values++)
 {
 putchar(letters % 10 + '0');
 putchar(numbers % 10 + '0');
 putchar(values % 10 + '0');
-if (letters < 7)
+if (letters < 7 && numbers < 8 and values < 9)
 {
 putchar(',');
 putchar(' ');

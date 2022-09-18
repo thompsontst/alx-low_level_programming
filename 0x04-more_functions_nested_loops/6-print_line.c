ce907770560174e5c96223dddb92705214bfe36b
anithomp <thompsonabove@gmail.com>
2022-09-18 20:49:24 +0100

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_line - entry point
+* main - entry point
+* @n: the integer that prints the line
+* Description: A function that prints 10 times the numbers,
+* numbers, followed by a new line.
+* print_alphabet_x10: print abc 10 times
+* Return: 0
+*/
+void print_line(int n)
+{
+int line;
+if ( n > 0)
+{
+for (line = 0; line <= 9; line++)
+
+_putchar('_');
+}
+_putchar('\n');
+
+}

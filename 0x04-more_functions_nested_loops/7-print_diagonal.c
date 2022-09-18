4609064ed0894f899778627dcf4c9677f2b3e2a2
anithomp <thompsonabove@gmail.com>
2022-09-18 20:57:00 +0100

    print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_diagonal - entry point
+* main - entry point
+* @n: the integer that prints the line
+* Description: A function that prints 10 times the numbers,
+* numbers, followed by a new line.
+* print_alphabet_x10: print abc 10 times
+* Return: 0
+*/
+void print_diagonal(int n)
+{
+int line;
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+
+_putchar('\');
+}
+_putchar('\n');
+
+}

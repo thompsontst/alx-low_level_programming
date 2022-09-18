7242b26d85e0a57c35a12e6c8a5355e414e05e6c
anithomp <thompsonabove@gmail.com>
2022-09-18 18:17:31 +0100

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+include <stdio.h>
+/**
+ * main - entry point
+ * Description: a program that prints all possible
+ * combinations of double-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+int letters;
+
+for (letters = 0; letters <= 99; letters++)
+{
+putchar(letters + '0');
+if (letters < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

d6fc17574159856df1a23f24b1f455e10c1d9da9
anithomp <thompsonabove@gmail.com>
2022-09-18 21:38:55 +0100

    print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_diagonal - entry point
+* main - entry point
+* @n: the integer that prints the line
+* Description: A function that prints 10 times the numbers,
+* numbers, followed by a new line.
+* print_alphabet_x10: print abc 10 times
+* Return: 0
+*/
+void print_square(int size)
+{
+int line, space;
+if (size <= 0)
+_putchar('\n');
+for (line = 0; line < size; line++)
+{
+for (space = 0; space <= line; space++)
+{
+if (line == space)
+{
+_putchar('#');
+_putchar('\n');
+}
+else
+{
+_putchar(' ');
+}
+}
+}
+}

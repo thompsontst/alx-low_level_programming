a7a182820e3449344421f299de9aad30aede2e40
anithomp <thompsonabove@gmail.com>
2022-09-18 19:57:46 +0100

    good

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: a program that prints all possible
+ * combinations of double-digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+int letters, numbers, values;
+
+for (letters = 0; letters <= 9; letters++)
+{
+for (numbers = letters + 1; numbers <= 9; numbers++)
+{
+for (values = letters + 1; values <= 9; values++)
+{
+putchar(letters % 10 + '0');
+putchar(numbers % 10 + '0');
+if (letters < 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

ab0df450d2657f8d785a3a616f9868ec0d74eac5
anithomp <thompsonabove@gmail.com>
2022-09-18 22:47:33 +0100

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_triangle - entry point
+* main - entry point
+* @size: the integer that prints the line
+* Description: A function that prints 10 times the numbers,
+* numbers, followed by a new line.
+* print_alphabet_x10: print abc 10 times
+* Return: 0
+*/
+void print_triangle(int size)
+{
+int line, space;
+if (size <= 0)
+_putchar('\n');
+for (line = 0; line < size; line++)
+{
+for (space = 0; space <= line; space++)
+
+_putchar('#');
+_putchar('\n');
+}
+}
